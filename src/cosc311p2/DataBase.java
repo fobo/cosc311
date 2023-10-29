8990a57a34510b367646aa9ceb21b31f2aaded46
Kevin Diehr <kddiehr@gmail.com>
2023-10-29 17:09:44 -0400

    Added delete method and changed find method, split it between two classes to implement a find method inside linked list class. Removed old ordered array code.

--- src/cosc311p2/DataBase.java
+++ src/cosc311p2/DataBase.java
@@ -1,41 +1,36 @@
 package cosc311p2;
 
+import java.util.Map;
 import java.util.Scanner;
+import cosc311p2.DoublyLinkedList;
 
-
-public class DataBase 
-{
+public class DataBase {
 	private DataBaseArray myDB;
-	private IndexArray ID, First, Last;
+	DoublyLinkedList ID, First, Last; 
 
-	public DataBase() 
-	{
+	public DataBase() {
+		// TODO: Change ID, First, and Last to linked lists
 		myDB = new DataBaseArray(100);
-		ID = new IndexArray(100);
-		First = new IndexArray(100);
-		Last = new IndexArray(100);
+		ID = new DoublyLinkedList();
+		First = new DoublyLinkedList();
+		Last = new DoublyLinkedList();
 	}
 
 	private int nextDBRec; // This gets used to track where in the database array, the current empty
 							// position is to insert data
 
-	
-	
-	
 	// Insert method
-	public void insertRecord(DataBaseRec record) 
-	{
+	public void insertRecord(DataBaseRec record) {
 
-		// Check to see if ID already exists in the ordered array
-		if (findRecord(record.getID()) == -1) 
-		{
+		// Check to see if ID already exists in the Linked List
+		if (findRecord(record.getID(), ID) == -1) {
 			DataBaseRec recordCopy = new DataBaseRec(record.getFname(), record.getLname(), record.getID());
 
 			// Create an IndexRec object for the record with ID, First Name, and Last Name
 			IndexRecArray idRec = new IndexRecArray(recordCopy.getID(), nextDBRec);
 			IndexRecArray firstRec = new IndexRecArray(recordCopy.getFname(), nextDBRec);
 			IndexRecArray lastRec = new IndexRecArray(recordCopy.getLname(), nextDBRec);
-			
+
 			// Insert the IndexRec objects into the respective Index arrays
 			ID.insert(idRec);
 			First.insert(firstRec);
@@ -46,37 +41,24 @@ public class DataBase
 
 			// Increment nextDBRec to track the next empty position
 			nextDBRec++;
-		} else 
-		{
+		} else {
 			System.out.println("Record with ID " + record.getID() + " already exists.");
 		}
 
 	}
 
-	//Builds the string for outputting data
+	// Builds the string for outputting data
 	@Override
-	public String toString() 
-	{
+	public String toString() {
 		StringBuilder stringBuilder = new StringBuilder();
-		for (int i = 0; i < nextDBRec; i++) 
-		{
+		for (int i = 0; i < nextDBRec; i++) {
 			stringBuilder.append("  ").append(myDB.dataBaseRecArray[i]).append("\n");
 		}
 		return stringBuilder.toString();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	// Modular List Method(s)
-	public void addIt() 
-	{
+	public void addIt() {
 		String input;
 		DataBaseRec dbr = new DataBaseRec();
 		Scanner scan = new Scanner(System.in);
@@ -86,13 +68,11 @@ public class DataBase
 
 		String[] data = input.split(" "); // Space separated values are stored into a String array
 
-		if (data.length == 3) 
-		{
+		if (data.length == 3) {
 			dbr.setData(data[0], data[1], data[2]);
 			insertRecord(dbr);
 			System.out.println("Record added successfully.");
-		} else 
-		{
+		} else {
 			System.out.println("Invalid input. Please provide First Name, Last Name, and ID separated by spaces.");
 		}
 		System.out.println("===Press enter to continue===");
@@ -100,78 +80,46 @@ public class DataBase
 
 	}
 
-	public void deleteIt() 
-	{
+	public void deleteIt() {
 		Scanner findID = new Scanner(System.in);
 		String input;
 		System.out.print("Enter the ID of the student you wish to remove: ");
 		input = findID.nextLine();
-		int where = findRecord(input);
+		int where = findRecord(input, ID);
+
 		
 		// Delete the found record
-		if (where != -1) 
-		{
+		if (where != -1) {
 			System.out.println("Record with ID " + myDB.dataBaseRecArray[where] + " has been deleted.");
 			ID.deleteIndexRecord(where);
 			First.deleteIndexRecord(where);
 			Last.deleteIndexRecord(where);
 
-		} else 
-		{
+		} else {
 			System.out.println("Record cannot be found.");
 		}
 
 	}
 
-	
-	
-	
-	// General binary search to find records. Is used for delete, find, and
-	// duplicates searches.
-	public int findRecord(String key) 
-	{
 
-		// Binary search
-		int left = 0;
-		int right = ID.getSize() - 1;
+	// TODO: Change to linear search for linked lists.
 
-		while (left <= right) 
-		{
+    public int findRecord(String key, DoublyLinkedList linkedList) {
+        int where = linkedList.findNode(key);
+        return where;
+    }
 
-			int mid = left + (right - left) / 2;
-			int comparison = ID.getIndexRecArray()[mid].getKey().compareTo(key);
-			if (comparison == 0) 
-			{
-				return ID.getIndexRecArray()[mid].getWhere();
-			} else if (comparison < 0) 
-			{
-				// The target ID is in the right half of the current range
-				left = mid + 1;
-			} else 
-			{
-				// The target ID is in the left half of the current range
-				right = mid - 1;
-			}
-
-		}
-		// default bad case, this means no record is found
-		return -1;
-	}
-
-	public void findIt() 
-	{
+	public void findIt() {
 
 		String input;
 
 		Scanner findID = new Scanner(System.in);
 		System.out.print("Enter the ID of the student you wish to find: ");
 		input = findID.nextLine();
-		int where = findRecord(input);
-		if (where == -1) 
-		{
+		int where = findRecord(input, ID);
+		if (where == -1) {
 			System.out.println("Record with ID " + input + " not found.");
-		} else 
-		{
+		} else {
 			System.out.println("Record found: " + myDB.dataBaseRecArray[where]);
 		}
 		System.out.println("===Press enter to continue===");
@@ -179,70 +127,45 @@ public class DataBase
 
 	}
 
-	public void listByField(String field, boolean ascending) 
-	{
-		IndexArray selectedArray;
+	public void listByField(String field, boolean ascending) {
+	    Map<String, DoublyLinkedList> fieldToLinkedList = Map.of("ID", ID, "First", First, "Last", Last);
 
-		if (field.equals("ID")) 
-		{
-			selectedArray = ID;
-		} else if (field.equals("First")) 
-		{
-			selectedArray = First;
-		} else if (field.equals("Last")) 
-		{
-			selectedArray = Last;
-		} else 
-		{
-			throw new IllegalArgumentException("Invalid field name");
-		}
+	    DoublyLinkedList selectedList = fieldToLinkedList.get(field);
+	    if (selectedList == null) {
+	        throw new IllegalArgumentException("Invalid field name");
+	    }
 
-		selectedArray.iteratorInitFront(); // Initialize the selected array
-
-		if (!ascending) 
-		{
-			selectedArray.iteratorInitBack(); // Initialize for descending order
-		}
-
-		// Selects which direction the iterator will flow
-		while (selectedArray.hasNext()) 
-		{
-			int where = ascending ? selectedArray.getNext() : selectedArray.getPrevious();
-			System.out.println(myDB.dataBaseRecArray[where].toString());
-		}
-
+	    if (ascending) {
+	        selectedList.displayForward();
+	    } else {
+	        selectedList.displayBackward();
+	    }
 	}
 
-	public void ListByIDAscending() 
-	{
+
+	public void ListByIDAscending() {
 		listByField("ID", true);
 	}
 
-	public void ListByFirstAscending() 
-	{
+	public void ListByFirstAscending() {
 		listByField("First", true);
 	}
 
-	public void ListByLastAscending() 
-	{
+	public void ListByLastAscending() {
 		listByField("Last", true);
 
 	}
 
-	
-	public void ListByIDDescending() 
-	{
+	public void ListByIDDescending() {
 		listByField("ID", false);
 
 	}
 
-	public void ListByFirstDescending() 
-	{
+	public void ListByFirstDescending() {
 		listByField("First", false);
 	}
 
-	public void ListByLastDescending() 
-	{
+	public void ListByLastDescending() {
 		listByField("Last", false);
 	}
 }
--- src/cosc311p2/DoublyLinkedList.java
+++ src/cosc311p2/DoublyLinkedList.java
@@ -5,7 +5,7 @@ package cosc311p2;
  */
 public class DoublyLinkedList {
 
-	private Node head; //Double Ended feature allows us to go directly to the front or end.
+	private Node head; // Double Ended feature allows us to go directly to the front or end.
 	private Node tail;
 	private int size;
 
@@ -22,37 +22,80 @@ public class DoublyLinkedList {
 	// insert first/last, delete first/last
 
 	// TODO: Add insert arbitrary position and delete where key
-	
-	public void insert(IndexRecArray indexRecord) {
-	    Node newNode = new Node(indexRecord);
 
-	    // If the LinkedList is empty or the new node should be inserted at the beginning
-	    if (head == null || indexRecord.compareTo(head.data) < 0) {
-	        newNode.next = head;
-	        newNode.prev = null;
-	        if (head != null) {
-	            head.prev = newNode;
-	        }
-	        head = newNode;
-	        size++;
-	        return;
-	    }
-
-	    // Find the position where the new node should be inserted
+	public int findNode(String key) {
 	    Node current = head;
-	    while (current.next != null && indexRecord.compareTo(current.next.data) > 0) {
+
+	    while (current != null) {
+	        if (current.data.getKey().equals(key)) {
+	            return current.data.getWhere();
+	        }
 	        current = current.next;
 	    }
 
-	    // Insert the new node
-	    newNode.next = current.next;
-	    newNode.prev = current;
-	    if (current.next != null) {
-	        current.next.prev = newNode;
-	    }
-	    current.next = newNode;
-	    size++;
+	    // Default bad case, no record found
+	    return -1;
 	}
+	
+	public void insert(IndexRecArray indexRecord) {
+		Node newNode = new Node(indexRecord);
+
+		// If the LinkedList is empty or the new node should be inserted at the
+		// beginning
+		if (head == null || indexRecord.compareTo(head.data) < 0) {
+			newNode.next = head;
+			newNode.prev = null;
+			if (head != null) {
+				head.prev = newNode;
+			}
+			head = newNode;
+			size++;
+			return;
+		}
+
+		// Find the position where the new node should be inserted
+		Node current = head;
+		while (current.next != null && indexRecord.compareTo(current.next.data) > 0) {
+			current = current.next;
+		}
+
+		// Insert the new node
+		newNode.next = current.next;
+		newNode.prev = current;
+		if (current.next != null) {
+			current.next.prev = newNode;
+		}
+		current.next = newNode;
+		size++;
+	}
+
+	public Node deleteIndexRecord(int where) {
+	    Node current = head; // start at the beginning
+
+	    while (current != null && current.data.compareTo(new IndexRecArray("", where)) != 0) {
+	        current = current.next; // move to the next node
+	    }
+
+
+	    if (current == null) {
+	        return null; // didn't find it, check for null on delete method to report to user.
+	    }
+
+	    if (current == head) {
+	        head = current.next; // first item; update head
+	    } else {
+	        current.prev.next = current.next; // not first; update previous node's next
+	    }
+
+	    if (current == tail) {
+	        tail = current.prev; // last item; update tail
+	    } else {
+	        current.next.prev = current.prev; // not last; update next node's previous
+	    }
+
+	    return current; // return the deleted node to print to user.
+	}
+
 
 	public void displayForward() {
 		System.out.print("List (first-->last): ");
